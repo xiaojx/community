048ff9608b184b2eb444a946a543c106cf83756d
unknown <2648134603@qq.com>
2022-11-02 22:38:28 +0800

    init repo

---
+++ src/main/java/com/jx/community/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.jx.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
